8ae2f9d33c35ef5f35556ed6b4b8a41e3f837b58
VitaliiFedan <vitalii@herdwatch.com>
2022-11-28 00:39:24 +0200

    add spring and security configurations

---
+++ src/main/java/org/vitalii/carrestservice/configurations/SecurityConfig.java
@@ -0,0 +1,25 @@
+package org.vitalii.carrestservice.configurations;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/api/v1/**").permitAll()
+                .anyRequest().authenticated();
+        return http.build();
+    }
+
+}
